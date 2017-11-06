82e2e6aedd5924f63779cfa3db8f609bbe4a4e12
Daniel Tovar <danieldaniyyelda@gmail.com>
2017-11-06 05:21:04 -0400

    try to connect

---
+++ COLSTRAT/COLSTRAT.Domain/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace COLSTRAT.Domain
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    [Table("Category", Schema = "public")]
+    public class Category
+    {
+        //[Key]
+        public int CategoryId { get; set; }
+
+       /* [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(50,ErrorMessage ="The field {0} only can contain {1} characters lenth")]
+        [Index("Category_Description_Index", IsUnique = true)]*/
+        public string Description { get; set; }
+        
+    }
+}
